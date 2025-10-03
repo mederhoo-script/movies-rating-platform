253b19c4bfa207bcd69a43c5709a3d2b84c95531
Hammed Amusat Oriyomi <mederhoo@gmail.com>
2025-10-03 23:08:02 +0100

    update

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import Navbar from './components/Navbar';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import MovieList from './pages/MovieList';
+import MovieDetail from './pages/MovieDetail';
+import AddMovie from './pages/AddMovie';
+import './App.css';
+
+function App() {
+  return (
+    <AuthProvider>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <main className="main-content">
+            <Routes>
+              <Route path="/" element={<MovieList />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/movies/:id" element={<MovieDetail />} />
+              <Route path="/movies/add" element={<AddMovie />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </AuthProvider>
+  );
+}
+
+export default App;
+
